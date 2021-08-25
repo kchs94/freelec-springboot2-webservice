d353e89d9eb159cea68d50600846d2d7da6a6caa
김충섭 <kchs94@gmail.com>
2021-08-25 17:29:02 +0900

    Add Application.java

---
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -0,0 +1,11 @@
+package com.jojoldu.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  // 스프링부트 자동설정
+public class Application {  // 프로젝트의 메인클래스
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
