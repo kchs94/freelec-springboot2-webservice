10a73fda4c90bde1da4a6f82c57a966fbc6a4629
김충섭 <kchs94@gmail.com>
2021-09-24 13:20:29 +0900

    Add 챕터4 게시글 수정, 삭제 화면 만들기

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -2,7 +2,11 @@ package com.jojoldu.book.springboot.domain.posts;
 // JpaRepository: DB Layer 접근자. 인터페이스로 생성한다.JpaRepositry<Entity 클래스, PK타입>을 상속하면 기본적인
 // CRUD 메소드가 자동으로 만들어진다.
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
